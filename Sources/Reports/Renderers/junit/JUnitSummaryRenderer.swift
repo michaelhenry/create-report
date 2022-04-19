f5c6be46c341bfad637772ed31b699332cd351ba
Michael Henry Pantaleon <me@iamkel.net>
2022-04-20 01:45:23 +1000

    separate SimpleXMLParser

--- Sources/Reports/Renderers/junit/JUnitSummaryRenderer.swift
+++ Sources/Reports/Renderers/junit/JUnitSummaryRenderer.swift
@@ -1,13 +1,14 @@
 import Foundation
+import SimpleXMLParser
 
 public struct JUnitSummaryRenderer: SummaryRenderer {
 
     private let path: String
-    
+
     public init(path: String) {
         self.path = path
     }
-    
+
     public func render() async throws -> String {
         let junitFile = try String(contentsOfFile: path)
         let xmlParser = SimpleXMLParser()
@@ -18,7 +19,7 @@ public struct JUnitSummaryRenderer: SummaryRenderer {
         }
         return md
     }
-    
+
     private func render(testSuites: Element) -> String {
         var md = ""
         testSuites.children.forEach { testSuite in
@@ -26,28 +27,28 @@ public struct JUnitSummaryRenderer: SummaryRenderer {
         }
         return md
     }
-    
+
     private func render(testSuite: Element) -> String {
         guard let name = testSuite.attributes["name"] else {
             return ""
         }
-        
+
         var md = """
-        
+
         ## \(name)
-        
+
         \(createRow(columns: ["&nbsp;", "Name", "Time (sec)", "Message"]))
         \(createRow(columns: ["---", ":---", "---:", "---"]))
-        
+
         """
-        
+
         testSuite.children.forEach { testCase in
             md += render(testCase: testCase) + "\n"
         }
         md += "\n"
         return md
     }
-    
+
     private func render(testCase: Element) -> String {
         guard let name = testCase.attributes["name"],
               let time = testCase.attributes["time"] else {
@@ -57,13 +58,13 @@ public struct JUnitSummaryRenderer: SummaryRenderer {
             .filter { $0.name.lowercased() == "failure" }
             .compactMap { $0.attributes["message"] }
             .joined()
-        
+
         let result = failureMessages.isEmpty ? "✅" : "❌"
         return """
         \(createRow(columns: [result, name, time, failureMessages]))
         """
     }
-    
+
     private func createRow(columns: [String]) -> String {
         return "| " + columns.joined(separator: " | ") + " |"
     }
